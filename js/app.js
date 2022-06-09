74f48b0084c988c3cf8500840df9358ef14ae17a
vrynerson <98570777+vrynerson@users.noreply.github.com>
2022-06-08 23:27:18 -0700

    create table

--- js/app.js
+++ js/app.js
@@ -6,5 +6,19 @@ var tbody = d3.select("tbody");
 
 // build a table
 function buildTable(data) {
+    // first celar out any existing data:
     tbody.html("");
+
+    // next, loop thru each object in the data and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+        // append a row to the table body
+        let row = tbody.append("tr");
+
+        //loop thru each field in the dataRow and add each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+            }
+        );
+    });
 }
